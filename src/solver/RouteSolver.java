921e7af67741106d88dddc3d0f28f37bf70ed980
Defoy <am01620@logti-a3412-15.ens.ad.etsmtl.ca>
2015-03-05 16:29:51 -0500

    solver

---
+++ src/solver/RouteSolver.java
@@ -0,0 +1,25 @@
+package solver;
+
+import model.RoutesModel;
+import model.ShortestRouteModel;
+
+public class RouteSolver implements Solver<RoutesModel, ShortestRouteModel>{
+
+	public RouteSolver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ShortestRouteModel solve(RoutesModel input) {
+		for(int i=0; i <= input.getnbSommet(); i++){
+			
+		}
+		return null;
+	}
+	private int getMinSommet(){
+		int position = 0;
+		return position;
+	}
+
+}
+ 
